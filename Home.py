3b09a2829ecef44c86dfd9293525fa58aebb8191
su2708 <46125751+su2708@users.noreply.github.com>
2024-11-10 19:04:53 +0900

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Hello world!")
+
+st.subheader("Welcome to Streamlit!")
+
+st.markdown(
+    """
+    ## I love it!
+    """
+)
+
+
+
+"""
+Home.py 실행 시 아래 커맨드 복붙
+"D:\Anaconda3_envs\Fullstack-GPT\python.exe" -m streamlit run home.py
+"""
