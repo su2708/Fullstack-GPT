6219be0fdcccccfff2774779f86ce4c8884f3f69
su2708 <46125751+su2708@users.noreply.github.com>
2024-11-11 15:48:19 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,10 +2,25 @@ import streamlit as st
 
 st.set_page_config(
     page_title="FullstackGPT Home",
-    page_icon="🖥️"
+    page_icon="🤖"
 )
 
-st.title("FullstackGPT Home")
+st.markdown(
+    """
+    # Hello!
+    
+    Welcome to my FullstackGPT Portfolio!
+    
+    Here are the apps I made:
+    
+    - [ ] [DocumentGPT](/DocumentGPT)
+    - [ ] [PrivateGPT](/PrivateGPT)
+    - [ ] [QuizGPT](/QuizGPT)
+    - [ ] [SiteGPT](/SiteGPT)
+    - [ ] [MeetingGPT](/MeetingGPT)
+    - [ ] [InvestorGPT](/InvestorGPT)
+    """
+)
 
 
 _ = """
