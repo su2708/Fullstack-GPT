03cfaebbf5407172037dd8490a4b55680958a155
su2708 <46125751+su2708@users.noreply.github.com>
2024-11-11 15:48:36 +0900

    Update basic settings

---
+++ pages/04_SiteGPT.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
+st.title("Site GPT")
---
+++ pages/06_InvestorGPT.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="InvestorGPT",
+    page_icon="💰",
+)
+
+st.title("Investor GPT")
