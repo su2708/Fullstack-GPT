1935e34a303268fc6f799cc118eb837034bc4ec4
su2708 <46125751+su2708@users.noreply.github.com>
2024-11-11 23:53:34 +0900

    Update 01_DocumentGPT.py

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -20,10 +20,41 @@ st.markdown(
     """
 )
 
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+        
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+
+    loader = UnstructuredFileLoader(file_path)
+
+    docs = loader.load_and_split(text_splitter=splitter)
+
+    embeddings = OpenAIEmbeddings()
+
+    # 중복 요청 시 캐시된 결과를 반환
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir
+    )
+    # FAISS 라이브러리로 캐시에서 임베딩 벡터 검색
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+
+    # docs를 불러오는 역할
+    retriever = vectorstore.as_retriever()
+    
+    return retriever
+
 file = st.file_uploader("Upload a .txt .pdf or .docx file", type=["pdf", "txt", "docx"])
 
 if file:
-    st.write(file)
-    file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
-    st.write(file_content, file_path)
+    retriever = embed_file(file)
+    s = retriever.invoke("winston")
+    s
