14533b667c0abd3d8a3d9d3c4df875f7d3b8a759
su2708 <46125751+su2708@users.noreply.github.com>
2024-11-11 15:48:22 +0900

    Update 01_DocumentGPT.py

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,31 @@
+import time
 import streamlit as st
 
-st.title("Document GPT")
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📖",
+)
+
+st.title("DocumentGPT")
+
+if "message" not in st.session_state:
+    st.session_state["message"] = []  # initialize a session_state["message"] list
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["message"].append({"message": message, "role": role})
+        
+for message in st.session_state["message"]:
+    send_message(message["message"], message["role"], save=False)
+    
+message = st.chat_input("Send a message to the ai")
+
+if message:
+    send_message(message, "human")
+    time.sleep(2)
+    send_message(f"You said: {message}", "ai")
+    
+    with st.sidebar:
+        st.write(st.session_state)
