90290a909b9bbb1adb078f3c0415c03cee7ea7e4
su2708 <46125751+su2708@users.noreply.github.com>
2025-02-28 00:43:33 +0900

    Update 11.7 Refine Chain

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -5,6 +5,16 @@ import glob
 import streamlit as st
 import os
 from pydub import AudioSegment
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import TextLoader
+from langchain_unstructured import UnstructuredLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain_community.vectorstores import FAISS
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
 
 st.set_page_config(
     page_title="MeetingGPT",
@@ -19,6 +29,10 @@ st.markdown(
     
     Get started by uploading a video file in the sidebar.
     """
+)
+
+llm = ChatOpenAI(
+    temperature=0.1,
 )
 
 has_transcript = os.path.exists("./.cache/itsub.txt")
@@ -65,16 +79,82 @@ with st.sidebar:
 
 if video:
     chunks_folder = "./.cache/chunks"
-    with st.status("Loading video..."):
+    with st.status("Loading video...") as status:
         video_content = video.read()
         video_path = f"./.cache/{video.name}"
         audio_path = video_path.replace("mp4", "mp3")
         transcript_path = video_path.replace("mp4", "txt")
         with open(video_path, "wb") as f:
             f.write(video_content)
-    with st.status("Extracting audio..."):
+        
+        status.update(label="Extracting audio...")
         extract_audio_from_video(video_path)
-    with st.status("Cutting audio segments..."):
+        
+        status.update(label="Cutting audio segments...")
         cut_audio_in_chunks(audio_path, 5, chunks_folder)
-    with st.status("Transcribing audio..."):
+        
+        status.update(label="Transcribing audio...")
         transcribe_chunks(chunks_folder, transcript_path)
+        
+    transcrip_tab, summary_tab, qa_tab = st.tabs(
+        [
+            "Transcript",
+            "Summary",
+            "Q&A",
+        ]
+    )
+    
+    with transcrip_tab:
+        with open(transcript_path, "r", encoding='utf-8') as file:
+            st.write(file.read())
+    
+    with summary_tab:
+        start = st.button("Generate summary")
+        
+        if start:
+            loader = UnstructuredLoader(transcript_path)
+            splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=800,
+                chunk_overlap=100,
+            )
+            docs = loader.load_and_split(text_splitter=splitter)
+            
+            first_summary_prompt = ChatPromptTemplate.from_template(
+                """ 
+                Write a concise summary of the following:
+                "{text}"
+                CONCISE SUMMARY:
+                """
+            )
+            
+            first_summary_chain = first_summary_prompt | llm | StrOutputParser()
+            
+            summary = first_summary_chain.invoke({
+                "text": docs[0].page_content
+            })
+            
+            refine_prompt = ChatPromptTemplate.from_template(
+                """ 
+                Your job is to produce a final summary.
+                We have provided an existing summary up to a certain point: {existing_summary}
+                
+                We have the opportunity to refine the existing summary (only if needed) with some more context below.
+                -----------
+                {text}
+                -----------
+                Given the new context, refine the original summary.
+                
+                If the context isn't useful, RETURN the original summary.
+                """
+            )
+            
+            refine_chain = refine_prompt | llm | StrOutputParser()
+            
+            with st.status("Summarizing...") as status:
+                for i, doc in enumerate(docs[1:]):
+                    status.update(label=f"Processing document {i+1}/{len(docs)-1}")
+                    summary = refine_chain.invoke({
+                        "existing_summary": summary,
+                        "text": doc.page_content,
+                    })
+            st.write(summary)
